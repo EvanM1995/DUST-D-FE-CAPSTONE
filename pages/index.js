e119322924e5ef8a2b9fe80fa0998d0676748a15
Evan Morgan <evanmorgan1995@gmail.com>
2023-09-18 21:34:14 -0500

    search bar and final touch ups

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { getAlbums } from '../api/albumData';
 import AlbumCard from '../components/AlbumCard';
 import { useAuth } from '../utils/context/authContext';
+import SearchBar from '../components/SearchBar';
 
 function Collection() {
   const [records, setRecords] = useState([]);
@@ -17,15 +18,30 @@ function Collection() {
     getAllTheRecords();
   }, []);
 
-  return (
-    <div id="records" className="text-center my-4">
-      <div className="d-flex flex-wrap">
-        {records.map((record) => (
-          <AlbumCard key={record.firebaseKey} albumObj={record} onUpdate={getAllTheRecords} />
-        ))}
+  const filterResult = (query) => {
+    if (!query) {
+      getAllTheRecords();
+    } else {
+      const filter = records.filter((record) => record.title.toLowerCase().includes(query));
+      setRecords(filter);
+    }
+  };
+
+    <div>
+      <SearchBar onKeyUp={(query) => filterResult(query)} />
+    </div>;
+
+    return (
+      <div id="records" className="text-center my-4">
+        <div> <SearchBar onKeyUp={(query) => filterResult(query)} />
+          <div className="d-flex flex-wrap">
+            {records.map((record) => (
+              <AlbumCard key={record.firebaseKey} albumObj={record} onUpdate={getAllTheRecords} />
+            ))}
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
 }
 
 export default Collection;
